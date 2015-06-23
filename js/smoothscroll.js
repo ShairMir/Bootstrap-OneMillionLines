ae6000e7d22333d491e3ccf4fbc49b750a892057
Shair Mir <s.mir89@hotmail.com>
2015-06-23 21:00:34 +0200

    Back to top button with relative positioning

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -31,3 +31,20 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
        window.location.hash = hash;
      });
 });
+
+$("#back-to-top a[href^='#']").on('click', function(e) {
+   // prevent default anchor click behavior
+   e.preventDefault();
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -50
+     }, 600, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+});
